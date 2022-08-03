4dba79f00e36654ee94c99b313b119043937445a
barbararui <cristina.barbara@aluno.ifsp.edu.br>
2022-08-03 11:54:04 -0300

    blog

--- core/mysql.php
+++ core/mysql.php
@@ -4,5 +4,49 @@ function insere(string $entidade, array $dados) : bool
 {
     $retorno = false;
     
-    foreach ($dados as $campo => ) 
+    foreach ($dados as $campo => $dado) 
+    {
+        $coringa[$campo] = '?';
+        $tipo[] = gettype($dado) [0];
+        $$campo = $dado;
+    }
+
+    $instrucao = insert($entidade, $coringa);
+
+    $conexao = conecta();
+
+    $stmt = mysqli_prepare($conexao, $instrucao);
+
+    eval('mysqli_stmt_bind_param($stmt, \'' . implode('',$tipo) . '\',$' . implode(', $', array_keys($dados)) . ');');
+
+
+    mysqli_stmt_execute($stmt);
+
+    $retorno = (boolean) mysqli_stmt_affected_rows($stmt);
+    $_SESSION['errors'] = mysqli_stmt_error_list($stmt);
+
+    mysqli_stmt_close($stmt);
+
+    desconecta($conexao);
+
+    return $retorno;
 }
+
+function atualiza(string $entidade, array $dados, array $criterio = []) : bool
+
+    $retorno = false;
+
+    foreach ($dado as $campo => $dado)
+    {
+        $coringa_dados[$campo] = '?';
+        $tipo[] = gettype($dado) [0];
+        $$campo = $dado;
+    }
+
+    foreach ($criterio as $expressao)
+    {
+        $dado = expressao[count($expressao) -1];
+
+        $tipo[] = gettype($dado) [0];
+        $expressao[count($expressao)]
+    }
