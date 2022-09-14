95882a0f36e4b8fc6c5caa8307e4006bc5722f6e
barbararui <cristina.barbara@aluno.ifsp.edu.br>
2022-09-14 12:12:20 -0300

    aaaaa

--- core/post_repositorio.php
+++ core/post_repositorio.php
@@ -3,7 +3,7 @@
 session_start();
 require_once '../includes/valida_login.php';
 require_once '../includes/funcoes.php';
-require_once 'conexao_mysql.php';
+require_once 'conexao_mysqli.php';
 require_once 'sql.php';
 require_once 'mysql.php';
 
